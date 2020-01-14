5a0e4338276a7a19407f275edf8d994084c95c22
andrzejkantor9 <andrzejkantor9@gmail.com>
2020-01-15 00:07:34 +0100

    hp regen and damage post process

--- Source/TestingGrounds/TestingGroundsHUD.cpp
+++ Source/TestingGrounds/TestingGroundsHUD.cpp
@@ -25,8 +25,8 @@ void ATestingGroundsHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X + 30.f),
-										   (Center.Y));
+	const FVector2D CrosshairDrawPosition( (Center.X + 40.f),
+										   (Center.Y+50.f));
 
 	// draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
